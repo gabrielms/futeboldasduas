841801b000ac2f8ea46fc6612b97728f4f9ca6a8
Gabriel <gabrielmatosdesouza@hotmail.com>
2013-01-05 18:54:32 -0200

    Heroku Changes

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module Futeboldasduas
     config.assets.version = '1.0'
 
     # Heroku configuration
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
