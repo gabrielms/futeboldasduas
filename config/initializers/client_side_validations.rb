568c64962a87937a36f396990d95f426998f3724
Gabriel <gabrielmatosdesouza@hotmail.com>
2013-01-05 19:32:04 -0200

    Heroku Changes

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -1,5 +1,3 @@
-# ClientSideValidations Initializer
-
 require 'client_side_validations/simple_form' if defined?(::SimpleForm)
 require 'client_side_validations/formtastic'  if defined?(::Formtastic)
 
